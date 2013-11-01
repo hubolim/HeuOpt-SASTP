7a6a04dfe417bef94c743e43967064ec59dc94bd
hubolim <hubolim@git.fake>
2013-11-01 23:28:05 +0100

    problem instances added, helpers package created, problem instance reader SASTProblem initialized

--- src/SASTPMain.java
+++ src/SASTPMain.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+
+import helpers.SASTProblem;
+
 
 public class SASTPMain {
 
@@ -7,6 +11,13 @@ public class SASTPMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		String inputLocation = "instances/sastp_10.prob";
+		try {
+			SASTProblem sastProblem = new SASTProblem(inputLocation);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
